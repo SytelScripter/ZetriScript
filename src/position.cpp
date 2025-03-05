4bad30a2f7b06a886005b47820ef95b6c630e15e
SytelScripter <80828488+SytelScripter@users.noreply.github.com>
2025-03-05 15:43:10 +0200

    bug fixes

--- src/position.cpp
+++ src/position.cpp
@@ -6,12 +6,12 @@ enum specialpos {
 };
 
 class Position {
-    private:
+    public:
     std::string xPos;
     std::string yPos;
     std::string zPos;
     std::string Col;
 
-    public:
+    Position(std::string xPos_, std::string yPos_, std::string zPos_) : xPos(xPos_), yPos(yPos_), zPos(zPos_) {}
     Position(std::string xPos_, std::string yPos_, std::string zPos_, std::string Col_) : xPos(xPos_), yPos(yPos_), zPos(zPos_), Col(Col_) {}
 }
