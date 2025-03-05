92e0414b44b430b2cc0171f2b935c44a6f4762eb
SytelScripter <80828488+SytelScripter@users.noreply.github.com>
2025-03-05 17:45:46 +0200

    bug fixes

--- src/position.cpp
+++ src/position.cpp
@@ -10,8 +10,8 @@ class Position {
     std::string xPos;
     std::string yPos;
     std::string zPos;
-    std::string Col;
+    // std::string Col;
 
     Position(std::string xPos_, std::string yPos_, std::string zPos_) : xPos(xPos_), yPos(yPos_), zPos(zPos_) {}
-    Position(std::string xPos_, std::string yPos_, std::string zPos_, std::string Col_) : xPos(xPos_), yPos(yPos_), zPos(zPos_), Col(Col_) {}
+    // Position(std::string xPos_, std::string yPos_, std::string zPos_, std::string Col_) : xPos(xPos_), yPos(yPos_), zPos(zPos_), Col(Col_) {}
 };
