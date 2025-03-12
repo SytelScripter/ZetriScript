e0a4673a98772b6964631cdd67bfdecf344b7853
SytelScripter <80828488+SytelScripter@users.noreply.github.com>
2025-03-12 15:36:54 +0200

    bug fixes

--- src/parser_.cpp
+++ src/parser_.cpp
@@ -209,7 +209,7 @@ class Parser {
             if (temp.has_value()) return temp.value();
             executed = make_unique<NodeVarAccess>();
             executed->var_name_tok = name;
-            result_exec->node = move(executed);
+            result_exec->node = move(convert_node<variant<unique_ptr<NodeVarAccess>, unique_ptr<NodeClassBuiltInt>>>(executed));
             return move(result_exec);
         }
         else if (is_tok_type(toktype::keyword) && tokens[idx+1].type == toktype::left_paren) {
@@ -218,7 +218,7 @@ class Parser {
             if (temp.has_value()) return temp.value();
             executed = make_unique<NodeClassBuiltIn>();
             executed->class_name_tok = node->class_name_tok;
-            result_exec->node = move(executed);
+            result_exec->node = move(convert_node<variant<unique_ptr<NodeVarAccess>, unique_ptr<NodeClassBuiltInt>>>(executed));
             return move(result_exec);
         }
 
